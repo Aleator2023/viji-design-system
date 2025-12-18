7e6929f1db6c9951c6ab9df147944749d0bf8fed
Alexander Krutov <your@email.com>
2025-12-18 13:48:53 +0200

    chore: turn storybook repo into consumable DS package

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { Button } from "./components/Button/Button";
+export type { ButtonProps } from "./components/Button/Button";
+
+export { ICONS } from "./components/icons";
+export type { IconName } from "./components/icons";
